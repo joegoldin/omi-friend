77171d62d3103467f26dc97e231f218208e87da0
joegoldin <joe@joegold.in>
2024-09-03 20:30:55 -0700

    misc

---
+++ app/devenv.nix
@@ -0,0 +1,11 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ git flutter cocoapods firebase-tools ];
+
+  languages.java.enable = true;
+}
